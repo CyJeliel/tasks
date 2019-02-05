53b854d81baf13ed01cb86278360d5a4f95d0701
cindy.albuquerque <cindy.albuquerque@192.168.43.155>
2019-02-05 10:26:31 +0000

    Removing unused project and updating hibernate properties

--- tasks/tasks-web/src/main/java/com/deloitte/tasks/web/config/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.deloitte.tasks.web.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}

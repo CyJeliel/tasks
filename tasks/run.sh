660edcdbe268759fac97ce93b9588c0dc8bb6c2f
cindy.albuquerque <cindy.albuquerque@192.168.43.155>
2019-02-05 21:43:49 +0000

    Script to automate deployment.

---
+++ tasks/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+
+cd Documents/
+
+
+mkdir app_tasks
+
+
+cd app_tasks/
+
+
+git clone https://github.com/CyJeliel/tasks.git cindy_tasks
+
+
+cd cindy_tasks/tasks/tasks-repository/
+
+
+docker build . -t database_tasks:latest
+
+
+cd ..
+
+
+docker-compose up
